24a2c6dcbde9628211c34d56ea46b48e0d96c6a2
David Mambou <david.mambou@gmail.com>
2022-08-12 19:14:58 +0900

    switch to esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,24 @@
+const railsEnv = process.env.RAILS_ENV || 'development'
+const optimize = railsEnv !== 'development'
+
+const path = require('path')
+const { default: importGlob } = require('esbuild-plugin-import-glob')
+const { sassPlugin } = require('esbuild-sass-plugin')
+
+require('esbuild')
+  .build({
+    entryPoints: [
+      './application.ts',
+    ],
+    bundle: true,
+    outdir: path.join(process.cwd(), 'app/assets/builds'),
+    absWorkingDir: path.join(process.cwd(), 'app/javascript'),
+    color: true,
+    minify: optimize,
+    sourcemap: true,
+    watch: process.argv.includes("--watch"),
+    plugins: [
+      importGlob(),
+      sassPlugin({ cache: true }),
+    ],
+  })
