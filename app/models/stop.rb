ddd80cd8f586abfaad77f992878a8349a20bce5c
David Mambou <david.mambou@gmail.com>
2022-08-15 12:04:35 +0900

    fix db: rename join table to stops_activities and remove constraint on stays must have a stop

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,6 +1,6 @@
 class Stop < ApplicationRecord
   has_and_belongs_to_many :transfers
-  has_and_belongs_to_many :activities
+  has_and_belongs_to_many :activities, join_table: :stops_activities
   belongs_to :stay
   belongs_to :trip
   # belongs_to :transfer
--- db/migrate/20220720181928_create_stops.rb
+++ db/migrate/20220720181928_create_stops.rb
@@ -1,6 +1,7 @@
 class CreateStops < ActiveRecord::Migration[7.0]
   def change
     create_table :stops do |t|
+      t.string :name
       t.references :trip, null: false, foreign_key: true
       t.date :start_day
       t.date :end_day
