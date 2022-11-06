d9f3399de65cc7a0edf1f9c019f30fed52ece3df
David Mambou <david.mambou@gmail.com>
2022-11-06 19:46:28 +0900

    feat: 🎸 check cz is working well

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -4,7 +4,6 @@ import App from "./components/App";
 
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLElement);
-// tokencomment
 
 document.addEventListener("DOMContentLoaded", () => {
   root.render(
