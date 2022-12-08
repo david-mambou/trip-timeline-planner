1275d564ddc39844cd41c2fadb4fe8b6d941f71b
David Mambou <david.mambou@gmail.com>
2022-12-08 15:37:44 +0900

    feat: 🎸 create trip

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,17 +1,34 @@
 class Api::TripsController < ApplicationController
-  before_action :set_trip, only: %i[show]
+  before_action :set_trip, only: %i[show destroy]
   def index
     @trips = Trip.all
     render json: @trips
+  end
+
+  def create
+    @trip = Trip.new(sanitized_params)
+    if @trip.save!
+      render json: @trip, status: :created
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
   end
 
   def show
     render json: @trip
   end
 
+  def destroy
+    @trip.destroy
+  end
+
   private
 
   def set_trip
     @trip = Trip.find(params[:id])
+  end
+
+  def sanitized_params
+    params.require(:trip).permit(:name)
   end
 end
--- app/javascript/components/TripPage.tsx
+++ app/javascript/components/TripPage.tsx
@@ -133,5 +133,7 @@ export default function TripPage() {
         </VStack>
       </>
     );
+  } else {
+    return <></>;
   }
 }
