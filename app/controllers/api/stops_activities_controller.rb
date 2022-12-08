122f9676178318173e8c596de6f79334915e2ad1
David Mambou <david.mambou@gmail.com>
2022-12-08 10:34:23 +0900

    refactor: 💡 delete stops activities controller

--- app/controllers/api/stops_activities_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::StopsActivitiesController < ApplicationController
-  def index
-    @stops_activities = StopsActivities.all
-    render json: @stops_activities
-  end
-end
-
-# render the right activities from stops controller instead
