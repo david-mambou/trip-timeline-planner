9ec1a7e177b5d855239c5e3bf5c6e4a87a2e7bd0
david-mambou <david.mambou@gmail.com>
2025-06-24 02:20:15 +0900

    fix: 🐛 add key

--- app/javascript/components/trips/TripDetails.tsx
+++ app/javascript/components/trips/TripDetails.tsx
@@ -22,21 +22,11 @@ export default function TripDetails({ stops, stays, activities, transfers }: Tri
             stop.id && (
               <StopCard
                 activities={activities[stop.id]}
+                key={stop.id}
                 stay={stays?.find((s) => s.id === stop.stay_id)}
                 stop={stop}
                 transfer={transfers?.find((t) => t.id === stop.outbound_id)}
               />
-              // <>
-              //   <h3 key={idx}>{stop.name}</h3>
-              //   <div>{`Start: ${stop.start_day}`}</div>
-              //   <div>{`End: ${stop.end_day}`}</div>
-              //   <div>Staying at: {stays?.find((s) => s.id === stop.stay_id)?.name}</div>
-              //   {activities[stop.id] && (
-              //     <div>Activities: {activities[stop.id].map((activity) => activity.name).join(", ")}</div>
-              //   )}
-              //   {stop.outbound_id && <div>Leaving by: {transfers?.find((t) => t.id === stop.outbound_id)?.mode}</div>}
-              //   <Button onClick={() => navigate(`./stops/${stop.id}/activities/add`)}>Add activity</Button>
-              // </>
             ),
         )}
       </VStack>
