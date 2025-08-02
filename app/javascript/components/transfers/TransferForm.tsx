848f4e3a3ebe18dac4592b7dc49227902c160533
david-mambou <david.mambou@gmail.com>
2025-08-03 05:08:53 +0900

    feat: 🎸 update/destroy transfer (UI)

--- app/javascript/components/transfers/TransferForm.tsx
+++ app/javascript/components/transfers/TransferForm.tsx
@@ -1,10 +1,12 @@
 import { Button, Input, Select } from "@chakra-ui/react";
-import { SyntheticEvent, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { SyntheticEvent, useEffect, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { isEmptyObject } from "~/javascript/helpers/helpers";
 import { Transfer, TransferMode } from "../trips/TripPage";
 import { Trip } from "../trips/Trips";
 import snakecaseKeys from "snakecase-keys";
+import camelcaseKeys from "camelcase-keys";
+import { format } from "date-fns";
 
 type TransferFormProps = {
   trip: Trip;
@@ -25,6 +27,8 @@ const modes: { value: TransferMode; label: string }[] = [
 export default function TransferForm({ trip }: TransferFormProps) {
   const navigate = useNavigate();
   const [selectedMode, setSelectedMode] = useState(modes[0] && modes[0].value);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [searchParams] = useSearchParams();
   const isOutboundOfStr = searchParams.get("isOutboundOf");
@@ -36,6 +40,32 @@ export default function TransferForm({ trip }: TransferFormProps) {
   if (isInboundOfStr) {
     isInboundOf = parseInt(isInboundOfStr);
   }
+
+  const [transfer, setTransfer] = useState<Transfer>();
+  const { transferId } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (!transferId) {
+      setIsLoading(false);
+      return;
+    }
+    const fetchData = async () => {
+      try {
+        const response = await window.fetch(`/api/transfers/${transferId}`);
+        if (!response.ok) throw Error(response.statusText);
+        const data = await response.json();
+        const camelcasedData = camelcaseKeys(data);
+        setTransfer(camelcasedData);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [transferId]);
 
   const createTransfer = async (transfer: CreateTransferRequest) => {
     const snakecasedTransfer = snakecaseKeys(transfer);
@@ -51,6 +81,27 @@ export default function TransferForm({ trip }: TransferFormProps) {
       if (!response.ok) throw Error(response.statusText);
 
       window.alert("Transfer added!");
+      navigate(`/trips/${trip.id}`);
+      navigate(0);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateTransfer = async (transfer: CreateTransferRequest) => {
+    const snakecasedTransfer = snakecaseKeys(transfer);
+    try {
+      const response = await window.fetch(`/api/transfers/${transferId}`, {
+        method: "PUT",
+        body: JSON.stringify(snakecasedTransfer),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) throw Error(response.statusText);
+
+      window.alert("Transfer updated!");
       navigate(`/trips/${trip.id}`);
       navigate(0);
     } catch (error) {
@@ -78,15 +129,23 @@ export default function TransferForm({ trip }: TransferFormProps) {
     });
 
     if (isEmptyObject(errors)) {
-      await createTransfer({
-        isOutboundOf,
-        isInboundOf,
-        mode: target.mode.value,
-        departureTime: new Date(target.departureTime.value),
-        arrivalTime: new Date(target.arrivalTime.value),
-        pickupPoint: target.pickupPoint.value,
-        price: target.price.value,
-      });
+      transferId
+        ? await updateTransfer({
+            mode: target.mode.value,
+            departureTime: new Date(target.departureTime.value),
+            arrivalTime: new Date(target.arrivalTime.value),
+            pickupPoint: target.pickupPoint.value,
+            price: target.price.value,
+          })
+        : await createTransfer({
+            isOutboundOf,
+            isInboundOf,
+            mode: target.mode.value,
+            departureTime: new Date(target.departureTime.value),
+            arrivalTime: new Date(target.arrivalTime.value),
+            pickupPoint: target.pickupPoint.value,
+            price: target.price.value,
+          });
     }
   };
 
@@ -102,6 +161,14 @@ export default function TransferForm({ trip }: TransferFormProps) {
     return errors;
   };
 
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -114,14 +181,22 @@ export default function TransferForm({ trip }: TransferFormProps) {
           ))}
         </Select>
         <label htmlFor="pickupPoint">Pickup point</label>
-        <Input name="pickupPoint"></Input>
+        <Input defaultValue={transfer?.pickupPoint} name="pickupPoint"></Input>
         <label htmlFor="departureTime">Departure time</label>
-        <Input name="departureTime" type="datetime-local" />
+        <Input
+          defaultValue={transfer && format(transfer?.departureTime, "yyyy-MM-dd'T'HH:mm")}
+          name="departureTime"
+          type="datetime-local"
+        />
         <label htmlFor="arrivalTime">Arrival time</label>
-        <Input name="arrivalTime" type="datetime-local" />
+        <Input
+          defaultValue={transfer && format(transfer?.arrivalTime, "yyyy-MM-dd'T'HH:mm")}
+          name="arrivalTime"
+          type="datetime-local"
+        />
         <label htmlFor="price">Price</label>
-        <Input name="price"></Input>
-        <Button type="submit">Add transfer</Button>
+        <Input defaultValue={transfer?.price} name="price"></Input>
+        <Button type="submit">{transferId ? "Update transfer" : "Add transfer"}</Button>
       </form>
       <Button onClick={() => navigate(`/trips/${trip.id}`)}>Back to Trip</Button>
     </>
