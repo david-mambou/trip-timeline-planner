17b8f60e2e57916f346b6208f25fbdfba29a3f97
david-mambou <david.mambou@gmail.com>
2025-08-03 19:38:33 +0900

    feat: 🎸 implement from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0, 20]
+  end
+end
+
 end
