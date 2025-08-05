ce45160709932ca1c690d5fdd2de49ce2f0b175f
david-mambou <david.mambou@gmail.com>
2025-08-05 21:26:29 +0900

    feat: 🎸 add PrivateRoute to allow acces to authed users only

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -4,15 +4,18 @@ import TripPage from "./trips/TripPage";
 import Trips from "./trips/Trips";
 import LoginForm from "./auth/LoginForm";
 import CreateUserForm from "./auth/CreateUserForm";
+import PrivateRoute from "./routes/PrivateRoute";
 
 export default function App() {
   return (
     <Routes>
       <Route path="login" element={<LoginForm />} />
       <Route path="register" element={<CreateUserForm />} />
-      <Route path="trips/new" element={<TripForm inputMode="create" />} />
-      <Route path="trips/:id/*" element={<TripPage />} />
-      <Route path="trips" element={<Trips />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="trips/new" element={<TripForm inputMode="create" />} />
+        <Route path="trips/:id/*" element={<TripPage />} />
+        <Route path="trips" element={<Trips />} />
+      </Route>
     </Routes>
   );
 }
