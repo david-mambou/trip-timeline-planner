d1bf9aafeb5f49f5dbaa73b9f728edfce69216b6
david-mambou <david.mambou@gmail.com>
2025-08-09 20:34:22 +0900

    feat: 🎸 updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_03_103907) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_05_195206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -75,6 +75,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_03_103907) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -95,4 +97,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_03_103907) do
   add_foreign_key "stops", "transfers", column: "inbound_id", on_delete: :nullify
   add_foreign_key "stops", "transfers", column: "outbound_id", on_delete: :nullify
   add_foreign_key "stops", "trips"
+  add_foreign_key "trips", "users"
 end
